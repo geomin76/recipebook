49aaf29a6a9f65a9ac0f8703337ead0a31e4e261
geomin76 <geom@vt.edu>
2020-10-22 17:11:41 -0400

    Changing Github link

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/geomin76/recipebook"
       className="github"
       aria-label="GitHub"
       target="_blank"
